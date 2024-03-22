17d583efb39f59c1b60a6d653635eb330dce3d55
botprzemek <info@botprzemek.pl>
2024-03-22 16:59:18 +0100

    feat(app): multi-tenant structure with tenancy identifier (string)

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,9 @@
 export default defineNuxtRouteMiddleware(() => {
+  const route = useRoute();
+  const path = route.fullPath.includes(route.params.tenancy as string) ? "" : route.params.tenancy + "/auth/login";
   const user: { status: boolean } = {
     status: useCookie<boolean>("auth").value,
   };
 
-  if (!user.status) return navigateTo("./../auth/login");
+  if (!user.status) return navigateTo(path);
 });
