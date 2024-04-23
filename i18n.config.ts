2dbf52fe0b1b3a55178a75767ab60d7db24fa209
botprzemek <info@botprzemek.pl>
2024-04-23 15:32:12 +0200

    New translations [match].vue (Multilingual)

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,6 +1,69 @@
-export default defineNuxtRouteMiddleware((to, _) => {
-  const {
-    token
-  } = to.query;
-  if (!token) return navigateTo("/auth/login");
-});
+<template>
+  <div class="flex flex-col">
+    <h1>
+    {{ useRoute().params?.match }}
+    </h1>
+
+    <h2>
+      Wynik
+    </h2>
+
+    <h2>
+      Drużyna 1
+    </h2>
+
+    Gracz 1
+    Gracz 2
+    Gracz 3
+    Gracz 4
+
+    <h2>
+      Drużyna 2
+    </h2>
+
+    Gracz 1
+    Gracz 2
+    Gracz 3
+    Gracz 4
+
+    <h2>
+      Kontrola
+    </h2>
+
+    <Button>
+      Rzut za 2
+    </Button>
+
+    <Button>
+      Rzut za 1
+    </Button>
+
+    <Button>
+      Zbiórka
+    </Button>
+
+    <Button>
+      Asysta
+    </Button>
+
+    <Button>
+      Blok
+    </Button>
+
+    <Button>
+      Przechwyt
+    </Button>
+
+    <Button>
+      Strata
+    </Button>
+
+    <Button>
+      Faul
+    </Button>
+
+    <Button>
+      Cofnij
+    </Button>
+  </div>
+</template>
