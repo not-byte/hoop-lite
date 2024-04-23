07055e570afce9f11d786db7774ca0a88901a9e4
botprzemek <info@botprzemek.pl>
2024-04-23 15:32:20 +0200

    New translations verify.get.ts (Multilingual)

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,55 +1,6 @@
-<script setup lang="ts">
-const routeName = getPath();
-</script>
-
-<template>
-  <section v-if="false" class="w-screen h-screen grid place-content-center">
-    Knurowski Streetball
-    <br />
-    Rejestracja drużyny
-    <br />
-    Nazwa
-    <br />
-    Kategoria
-    <br />
-    Miasto
-    <br />
-    Gracz 1
-    <br />
-    Imię
-    <br />
-    Nazwisko
-    <br />
-    Pseudonim
-    <br />
-    Wiek
-    <br />
-    Gracz 2
-    <br />
-    Imię
-    <br />
-    Nazwisko
-    <br />
-    Pseudonim
-    <br />
-    Wiek
-    <br />
-    Gracz 3
-    <br />
-    Imię
-    <br />
-    Nazwisko
-    <br />
-    Pseudonim
-    <br />
-    Wiek
-    <br />
-    Adres e-mail kapitana
-    <br />
-    Numer telefonu kapitana
-    <br />
-    <ButtonBase>
-      Zatwierdź
-    </ButtonBase>
-  </section>
-</template>
+export default defineEventHandler(async event => {
+  const token = event.headers.get("authorization");
+  return {
+    authorized: token === "123"
+  };
+});
