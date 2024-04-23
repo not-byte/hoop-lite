755f494c0d3d0e6c61b7382da5ce7c2297ac7a25
botprzemek <info@botprzemek.pl>
2024-04-23 15:32:22 +0200

    New translations validatedata.ts (Multilingual)

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,25 +1,12 @@
-import type { Config } from "tailwindcss";
-export default (<Partial<Config>> {
-  theme: {
-    extend: {
-      fontFamily: {
-        display: "Inter"
-      },
-      fontSize: {
-        xxl: "2.986rem",
-        xl: "1.728rem",
-        lg: "1.2rem",
-        md: "1rem",
-        sm: "0.833rem"
-      },
-      colors: {
-        blood: "rgb(231,36,36)",
-        dark: "rgb(30,30,30,1.00)",
-        gray: "rgb(75,75,75,1.00)"
-      },
-      borderWidth: {
-        "1": "1px"
-      }
-    }
-  }
-});
+type Payload = LoginPayload | RegisterPayload | ResetPayload | RecoveryPayload | VerifyPayload;
+const regex: {
+  [key: string]: RegExp;
+} = {
+  first_name: /^([a-zA-ZąćęłńóżźĄĆĘŁŃÓŻŹ]){3,32}$/,
+  last_name: /^([a-zA-ZąćęłńóżźĄĆĘŁŃÓŻŹ]){3,32}$/,
+  email: /^[\w-.]+@([\w-]+\.)+[\w-]{2,6}$/,
+  password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,32}$/,
+  repeated_password: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,32}$/,
+  token: /^([a-zA-Z0-9!#$%^&*_]){8}$/
+};
+export default ((data: Payload) => Object.keys(data).every(key => regex[key] ? regex[key].test((data as any)[key]) : false));
