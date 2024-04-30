0516a0eda1058fcd700e7f1a9f0baa29782bee89
botprzemek <info@botprzemek.pl>
2024-04-30 15:33:40 +0200

    New translations index.vue (Multilingual)

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,11 +1,76 @@
 <script setup lang="ts">
-const { link } = defineProps([ "link" ]);
+const routeName = getPath();
+const data = ref<FormPayload>({
+  email: "",
+});
+
+const onTyping = (key: string, value: any) => (data.value[key as keyof Object] = value);
+
+const registration = async () => {
+  if (!validateData(data.value)) return;
+
+  const response = await $fetch("/api/data/teams", {
+    method: "PUT",
+    body: data.value,
+  });
+
+  if (!response) return;
+
+  return navigateTo("/dashboard");
+};
 </script>
 
 <template>
-  <NuxtLink :to="link" class="w-full bg-blood rounded-lg py-3 px-6 text-center">
-    <span class="text-white font-bold">
-      <slot/>
-    </span>
-  </NuxtLink>
+  <section class="w-screen h-screen grid place-content-center">
+    <form @submit.prevent="registration()">
+      <InputBase/>
+    </form>
+    Knurowski Streetball
+    <br />
+    Rejestracja drużyny
+    <br />
+    Nazwa
+    <br />
+    Kategoria
+    <br />
+    Miasto
+    <br />
+    Gracz 1
+    <br />
+    Imię
+    <br />
+    Nazwisko
+    <br />
+    Pseudonim
+    <br />
+    Wiek
+    <br />
+    Gracz 2
+    <br />
+    Imię
+    <br />
+    Nazwisko
+    <br />
+    Pseudonim
+    <br />
+    Wiek
+    <br />
+    Gracz 3
+    <br />
+    Imię
+    <br />
+    Nazwisko
+    <br />
+    Pseudonim
+    <br />
+    Wiek
+    <br />
+    Adres e-mail kapitana
+    <br />
+    Numer telefonu kapitana
+    <br />
+    <ButtonBase>
+      Zatwierdź
+    </ButtonBase>
+  </section>
 </template>
