8cbbddd1bfa0a58bd328b4dcfbd86462ff4a0773
botprzemek <info@botprzemek.pl>
2024-05-04 13:55:31 +0200

    New translations register.vue (Multilingual)

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,32 +1,27 @@
-<script setup async lang="ts">
-definePageMeta({
-  middleware: ["query"],
-});
-
+<script setup lang="ts">
 const routeName = getPath();
-const data = ref<RecoveryPayload>({
+const data = ref<RegisterPayload>({
+  first_name: "",
+  last_name: "",
+  email: "",
   password: "",
   repeated_password: "",
 });
-const { token } = useRoute().query;
 
 const onTyping = (key: string, value: any) => (data.value[key as keyof Object] = value);
 
-if (Array.isArray(token)) navigateTo("/auth/register");
-
-const recovery = async () => {
+const register = async () => {
   if (!validateData(data.value)) return;
 
-  const response = await $fetch("/api/auth/recovery", {
-      method: "POST",
-      body: data.value,
-    }
-  );
+  const response = await $fetch("/api/auth/register", {
+    method: "POST",
+    body: data.value,
+  });
 
-  console.table(response);
+  if (!response) return;
 
   return navigateTo("/auth/login");
-};
+}
 </script>
 
 <template>
@@ -40,9 +35,17 @@ const recovery = async () => {
         :route="routeName"
         :placeholder="key"
       />
-      <ButtonBase @click="recovery()">
+      <ButtonBase @click="register()">
         {{ $t(`routes.${routeName}.content.form.submit`) }}
       </ButtonBase>
     </form>
+    <aside class="mt-4 px-12">
+      <p class="text-high text-sm text-center">
+        {{ $t(`routes.${routeName}.content.aside.title`) }}
+        <NuxtLink to="/auth/login" class="text-blood underline">
+          {{ $t(`routes.${routeName}.content.aside.description`) }}
+        </NuxtLink>
+      </p>
+    </aside>
   </section>
 </template>
