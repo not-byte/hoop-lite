0bddeabac8d4c71d90473685a101ee947886f2d9
botprzemek <info@botprzemek.pl>
2024-05-04 13:55:33 +0200

    New translations index.vue (Multilingual)

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,45 +1,102 @@
 <script setup lang="ts">
-const routeName = getPath();
-const data = ref<ResetPayload>({
-  email: "",
-});
-
-const onTyping = (key: string, value: any) => (data.value[key as keyof Object] = value);
-
-const reset = async () => {
-  if (!validateData(data.value)) return;
-
-  const response = await $fetch("/api/auth/reset", {
-    method: "POST",
-    body: data.value,
-  });
-
-  if (!response) return;
-
-  return navigateTo("/auth/login");
-}
+const time = ref<string[]>(
+    formatHours(new Date("2024-05-05T12:00:00.000+02:00"))
+)
+// const routeName = getPath();
+// const data = ref<FormPayload>({
+//   email: "",
+// });
+//
+// const onTyping = (key: string, value: any) => (data.value[key as keyof Object] = value);
+//
+// const registration = async () => {
+//   if (!validateData(data.value)) return;
+//
+//   const response = await $fetch("/api/data/teams", {
+//     method: "PUT",
+//     body: data.value,
+//   });
+//
+//   if (!response) return;
+//
+//   return navigateTo("/dashboard");
+// };
+onMounted(() => {
+  setInterval(() => {
+    time.value = formatHours(new Date("2024-05-05T12:00:00.000+02:00"))
+  }, 60 * 1000)
+})
 </script>
 
 <template>
-  <section  class="flex flex-col sm:max-w-48 md:max-w-[18rem] lg:max-w-[24rem]">
-    <HeaderForm />
-    <form @submit.prevent class="w-full grid grid-flow-row gap-3">
-      <InputBase
-        v-for="(value, key) in data"
-        :key="key"
-        @typing="onTyping"
-        :route="routeName"
-        :placeholder="key"
-      />
-      <p class="text-high text-sm text-right">
-        {{ $t(`routes.${routeName}.content.form.forgot`) }}
-        <NuxtLink to="/auth/login" class="text-blood underline">
-          {{ $t(`routes.${routeName}.content.form.reset`) }}
-        </NuxtLink>
-      </p>
-      <ButtonLink @click="reset()">
-        {{ $t(`routes.${routeName}.content.form.submit`) }}
-      </ButtonLink>
-    </form>
+  <section class="w-screen h-screen grid place-content-center font-body uppercase">
+    <aside class="relative w-full h-20">
+      <IconHeader class="h-fit"/>
+      <IconLogo class="absolute top-0 left-[50%] -translate-x-1/2 -translate-y-16 h-48"/>
+      <span class="absolute top-0 left-[24%] -translate-x-1/2 translate-y-3 font-bold text-white text-[3rem]">
+        {{ time[0] }}H
+      </span>
+      <span class="absolute top-0 right-[23%] translate-x-1/2 translate-y-3 font-bold text-white text-[3rem]">
+        {{ time[1] }}M
+      </span>
+    </aside>
+    <h1 class="text-[2rem] font-bold text-center mt-12">
+      Do otwarcia rejestracji
+    </h1>
+    <span class="text-3xl text-white tracking-widest bg-blood p-3 text-center mt-4">
+      Knurowski Streetball
+    </span>
   </section>
+<!--  <section class="w-screen h-screen grid place-content-center">-->
+<!--    <form @submit.prevent="registration()">-->
+<!--      <InputBase/>-->
+<!--    </form>-->
+<!--    Knurowski Streetball-->
+<!--    <br />-->
+<!--    Rejestracja drużyny-->
+<!--    <br />-->
+<!--    Nazwa-->
+<!--    <br />-->
+<!--    Kategoria-->
+<!--    <br />-->
+<!--    Miasto-->
+<!--    <br />-->
+<!--    Gracz 1-->
+<!--    <br />-->
+<!--    Imię-->
+<!--    <br />-->
+<!--    Nazwisko-->
+<!--    <br />-->
+<!--    Pseudonim-->
+<!--    <br />-->
+<!--    Wiek-->
+<!--    <br />-->
+<!--    Gracz 2-->
+<!--    <br />-->
+<!--    Imię-->
+<!--    <br />-->
+<!--    Nazwisko-->
+<!--    <br />-->
+<!--    Pseudonim-->
+<!--    <br />-->
+<!--    Wiek-->
+<!--    <br />-->
+<!--    Gracz 3-->
+<!--    <br />-->
+<!--    Imię-->
+<!--    <br />-->
+<!--    Nazwisko-->
+<!--    <br />-->
+<!--    Pseudonim-->
+<!--    <br />-->
+<!--    Wiek-->
+<!--    <br />-->
+<!--    Adres e-mail kapitana-->
+<!--    <br />-->
+<!--    Numer telefonu kapitana-->
+<!--    <br />-->
+<!--    <ButtonBase>-->
+<!--      Zatwierdź-->
+<!--    </ButtonBase>-->
+<!--  </section>-->
 </template>
