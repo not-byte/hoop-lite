4a5daa65f24ff1f6e613858552d9d043205c3955
botprzemek <info@botprzemek.pl>
2024-05-05 12:05:34 +0200

    build(app): added fonts

--- utils/formatHours.ts
+++ utils/formatHours.ts
@@ -9,6 +9,13 @@ export default (time: Date): string[] => {
   const currentTime = new Date();
   const timeDifference = time.getTime() - currentTime.getTime();
 
+  if (timeDifference < 0) {
+    return [
+      '00',
+      '00',
+    ]
+  }
+
   const hours = Math.floor(timeDifference / (1000 * 60 * 60));
   const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
 
