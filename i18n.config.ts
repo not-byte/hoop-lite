cdb58bc6a6f00168ce28f0fcb8537f3bdaf05a6d
botprzemek <info@botprzemek.pl>
2024-05-05 14:00:54 +0200

    New translations dashboard.vue (Multilingual)

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,52 +1,61 @@
 <script setup lang="ts">
-const routeName = getPath();
-const data = ref<LoginPayload>({
-  email: "",
-  password: "",
+definePageMeta({
+  middleware: ["auth"],
 });
 
-const onTyping = (key: string, value: any) => (data.value[key as keyof Object] = value);
+const config = useRuntimeConfig();
+const routes = [
+  "teams",
+  "standings",
+  "live",
+  "schedule",
+  "leaderboard",
+  "players",
+];
 
-const login = async () => {
-  if (!validateData(data.value)) return;
+const logout = (to?: string) => {
+  useCookie("token", { sameSite: true }).value = null;
 
-  const response = await $fetch("/api/auth/login", {
-    method: "POST",
-    body: data.value,
-  });
-
-  if (!response || !response.token) return;
-
-  const expires: Date = new Date(new Date().getTime() + 1000 * 3600 * 24 * 30);
-
-  useCookie<string>("token", { sameSite: true, expires: expires }).value = response.token;
-
-  return navigateTo("/dashboard");
+  navigateTo(to ? to : "/auth/login");
 };
 </script>
 
 <template>
-  <section class="flex flex-col sm:max-w-48 md:max-w-[18rem] lg:max-w-[24rem]">
-    <HeaderForm />
-    <form @submit.prevent class="w-full grid grid-flow-row gap-3">
-      <InputBase v-for="(value, key) in data" :key="key" @typing="onTyping" :route="routeName" :placeholder="key" />
-      <p class="text-high text-sm text-right">
-        {{ $t(`routes.${routeName}.content.form.forgot`) }}
-        <NuxtLink to="/auth/reset" class="text-blood underline">
-          {{ $t(`routes.${routeName}.content.form.reset`) }}
+  <section class="relative w-screen h-screen grid grid-rows-1 grid-cols-8 gap-4">
+    <aside class="flex flex-col col-span-1 gap-4 bg-high/10">
+      <header class="w-full bg-blood">
+        <h3>
+          {{ config.public.name }}
+        </h3>
+        <NuxtLink
+          external
+          target="_blank"
+          rel="noreferrer"
+          to="https://github.com/not-byte/tournament-app"
+        >
+          {{ config.public.version }}
         </NuxtLink>
-      </p>
-      <ButtonBase @click="login()">
-        {{ $t(`routes.${routeName}.content.form.submit`) }}
-      </ButtonBase>
-    </form>
-    <aside class="mt-3">
-      <p class="text-high text-sm text-center">
-        {{ $t(`routes.${routeName}.content.aside.title`) }}
-        <NuxtLink to="/auth/register" class="text-blood underline">
-          {{ $t(`routes.${routeName}.content.aside.description`) }}
-        </NuxtLink>
-      </p>
+      </header>
+      <nav class="flex-grow">
+        <ul class="flex flex-col gap-4">
+          <li class="flex flex-row gap-2 bg-mid/25">
+            <IconUser/>
+            <NuxtLink to="/dashboard" class="text-white font-bold">
+              {{ $t(`routes.dashboard.name`) }}
+            </NuxtLink>
+          </li>
+          <li v-for="route in routes" :key="route" class="flex flex-row gap-2">
+            <IconUser/>
+            <NuxtLink :to="`/dashboard/${route}`">
+              {{ $t(`routes.dashboard.children.${route}.name`) }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+      <ButtonAccount/>
     </aside>
+    <section class="w-full h-full grid grid-flow-row col-start-2 col-end-9">
+      <NuxtPage />
+    </section>
   </section>
 </template>
