b623936302295b0cd5ef5c37eace32e5857a741e
botprzemek <info@botprzemek.pl>
2024-05-05 15:09:04 +0200

    New translations index.d.ts (Multilingual)

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,11 +1,52 @@
-<script setup lang="ts">
-const { link } = defineProps([ "link" ]);
-</script>
-
-<template>
-  <NuxtLink :to="link" class="w-full bg-blood rounded-lg py-3 px-6 text-center">
-    <span class="text-white font-bold">
-      <slot/>
-    </span>
-  </NuxtLink>
-</template>
+declare global {
+  enum Category {
+    OPEN,
+    SCHOOL,
+  }
+  interface PlayerPayload {
+    first_name: string;
+    last_name: string;
+    age?: number;
+  }
+  interface FormPayload {
+    name: string;
+    category?: Category;
+    email: string;
+    phone?: number;
+    players: PlayerPayload[];
+  }
+  interface LoginPayload {
+    email: string;
+    password: string;
+  }
+  interface RegisterPayload extends LoginPayload {
+    first_name: string;
+    last_name: string;
+    repeated_password: string;
+  }
+  interface ResetPayload {
+    email: string;
+  }
+  interface RecoveryPayload {
+    password: string;
+    repeated_password: string;
+  }
+  enum VerifyStatus {
+    SUCCESS,
+    INVALID,
+    ERROR,
+  }
+  interface VerifyPayload {
+    token: string;
+  }
+  enum InviteStatus {
+    SUCCESS,
+    INVALID,
+    ERROR,
+    AWAIT,
+  }
+  interface InvitePayload {
+    token: string;
+  }
+}
+export {};
