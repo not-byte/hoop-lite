733151886b1e091d9a14d3335120b8205de4e036
botprzemek <info@botprzemek.pl>
2024-05-05 15:09:03 +0200

    New translations link.vue (Multilingual)

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+const { link } = defineProps([ "link" ]);
+</script>
+
 <template>
-  <button class="w-full h-12 bg-blood rounded-lg py-3 px-6 text-center">
+  <NuxtLink :to="link" class="w-full bg-blood rounded-lg py-3 px-6 text-center">
     <span class="text-white font-bold">
-      <slot />
+      <slot/>
     </span>
-  </button>
+  </NuxtLink>
 </template>
