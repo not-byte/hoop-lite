036ceea57a7d40ba890b68f6d41b0f6f3ba35ddb
botprzemek <info@botprzemek.pl>
2024-05-05 15:08:59 +0200

    New translations tailwind.config.ts (Multilingual)

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,6 +1,30 @@
-export default defineEventHandler(async event => {
-  const token = event.headers.get("authorization");
-  return {
-    authorized: token === "123"
-  };
+import type { Config } from "tailwindcss";
+export default (<Partial<Config>> {
+  theme: {
+    extend: {
+      fontFamily: {
+        display: "Inter",
+        body: "Monument"
+      },
+      fontSize: {
+        xxl: "2.986rem",
+        xl: "1.728rem",
+        lg: "1.2rem",
+        md: "1rem",
+        sm: "0.833rem"
+      },
+      colors: {
+        blood: "rgba(242,53,53,1.00)",
+        dark: "rgba(30,30,30,1.00)",
+        gray: "rgba(75,75,75,1.00)",
+        mid: "rgba(125,125,125,1.00)"
+      },
+      borderWidth: {
+        "1": "1px"
+      },
+      gridTemplateColumns: {
+        "13": "repeat(13, minmax(0, 1fr))"
+      }
+    }
+  }
 });
