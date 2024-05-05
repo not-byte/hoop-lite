ed18b435707ccb538ff9dad87ed77c3e75541d18
botprzemek <info@botprzemek.pl>
2024-05-05 15:09:07 +0200

    New translations app.vue (Multilingual)

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,8 +1,58 @@
-export default ((sec: number): string => {
-  if (!sec) return '00:00';
-  const min: number = Math.floor(sec / 60);
-  const secLeft: number = sec % 60;
-  const newMin: string = min < 10 ? `0${min}` : `${min}`;
-  const newSec: string = secLeft < 10 ? `0${secLeft}` : `${secLeft}`;
-  return `${newMin}:${newSec}`;
+<script setup async lang="ts">
+const config = useRuntimeConfig();
+const routeName = getPath();
+const { t, locale } = useI18n({ useScope: "global" });
+
+useHeadSafe({
+  titleTemplate: (titleChunk) =>
+    titleChunk ? `${titleChunk} · ${config.public.name}` : `${t(`routes.${routeName}.name`)} · ${config.public.name}`,
+  htmlAttrs: {
+    lang: locale.value,
+  },
+  meta: [
+    {
+      name: "description",
+      content: `${t(`routes.${routeName}.description`)}`,
+    },
+  ],
 });
+
+useSeoMeta({
+  ogTitle: `${t(`routes.${routeName}.name`)} · ${config.public.name}`,
+  ogDescription: `${t(`routes.${routeName}.description`)}`,
+  ogImage: "[og:image]",
+  ogUrl: "[og:url]",
+  twitterTitle: "[twitter:title]",
+  twitterDescription: "[twitter:description]",
+  twitterImage: "[twitter:image]",
+  twitterCard: "summary",
+});
+</script>
+
+<template>
+  <VitePwaManifest />
+  <NuxtLoadingIndicator />
+  <NuxtPage />
+</template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition:
+    opacity 500ms ease-in-out,
+    transform 750ms;
+}
+
+.page-enter-active {
+  transition-delay: 250ms;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
+
+.page-enter-from {
+  transform: translateY(-2rem);
+}
+</style>
