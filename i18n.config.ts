7cd50c3c957ec77640512e2fb57575dd4fb9ea18
botprzemek <info@botprzemek.pl>
2024-05-05 15:08:55 +0200

    New translations recovery.vue (Multilingual)

--- i18n.config.ts
+++ i18n.config.ts
@@ -4,41 +4,45 @@ definePageMeta({
 });
 
 const routeName = getPath();
+const data = ref<RecoveryPayload>({
+  password: "",
+  repeated_password: "",
+});
 const { token } = useRoute().query;
-let status = false;
 
-status = Array.isArray(token);
+const onTyping = (key: string, value: any) => (data.value[key as keyof Object] = value);
 
-const { data: response } = await useFetch("/api/auth/verify", {
-    headers: {
-      authorization: token as string,
-    },
-  },
-);
+if (Array.isArray(token)) navigateTo("/auth/register");
 
-status = response.value ? response.value.authorized : false;
+const recovery = async () => {
+  if (!validateData(data.value)) return;
+
+  const response = await $fetch("/api/auth/recovery", {
+      method: "POST",
+      body: data.value,
+    }
+  );
+
+  console.table(response);
+
+  return navigateTo("/auth/login");
+};
 </script>
 
 <template>
-  <section class="flex flex-col sm:max-w-48 md:max-w-[18rem] lg:max-w-[24rem]">
-    <header class="place-content-center mb-6 text-center">
-      <h1>
-        {{ $t(`routes.${routeName}.content.${status}.content.header.title`) }}
-      </h1>
-      <p>
-        {{ $t(`routes.${routeName}.content.${status}.content.header.description`) }}
-      </p>
-    </header>
-    <ButtonLink link="/auth/register">
-      {{ $t(`routes.${routeName}.content.${status}.content.button`) }}
-    </ButtonLink>
-    <aside class="mt-3">
-      <p class="text-high text-sm text-center">
-        {{ $t(`routes.${routeName}.content.${status}.content.aside.title`) }}
-        <NuxtLink to="/auth/register" class="text-blood underline">
-          {{ $t(`routes.${routeName}.content.${status}.content.aside.description`) }}
-        </NuxtLink>
-      </p>
-    </aside>
+  <section  class="flex flex-col sm:max-w-48 md:max-w-[18rem] lg:max-w-[24rem]">
+    <HeaderForm />
+    <form @submit.prevent class="w-full grid grid-flow-row gap-3">
+      <InputBase
+        v-for="(value, key) in data"
+        :key="key"
+        @typing="onTyping"
+        :route="routeName"
+        :placeholder="key"
+      />
+      <ButtonBase @click="recovery()">
+        {{ $t(`routes.${routeName}.content.form.submit`) }}
+      </ButtonBase>
+    </form>
   </section>
 </template>
