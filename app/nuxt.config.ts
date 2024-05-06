71b2a617383566c46fbc2f73d6ecc77e6626cd79
Przemysław Szymański <przemyslaw.szymanski@flytronic.pl>
2024-05-06 12:21:27 +0200

    build(app): removed testing dependencies from production

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -12,19 +12,18 @@ export default defineNuxtConfig({
     mailUlrl: "",
     public: {
       name: "Knury Knurów",
-      version: "0.1.0",
+      version: "1.0.0",
       authorName: "notByte",
       authorUrl: "https://notByte.com/",
     },
   },
   modules: [
-    "@nuxt/test-utils/module",
     "@nuxt/image",
     "@nuxtjs/device",
     "@nuxtjs/i18n",
     "@nuxtjs/tailwindcss",
-    "nuxt-simple-sitemap",
     "@vite-pwa/nuxt",
+    "nuxt-simple-sitemap",
   ],
   i18n: {
     baseUrl: process.env.APP_URL || "http://localhost:3000/",
@@ -47,16 +46,14 @@ export default defineNuxtConfig({
     rootId: "tournament-app",
     rootTag: "main",
     teleportTag: "aside",
-    pageTransition: { name: "page", mode: "out-in" },
+    pageTransition: {
+      name: "page",
+      mode: "out-in"
+    },
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
       meta: [
-        {
-          name: "description",
-          content:
-            "Do you remember the first edition of the Knurowski Streetball tournament? We do too and we want to share some good news with you 🥳 On May 25th, we are organizing another installment of our basketball event for YOU.",
-        },
         {
           name: "author",
           content: "notbyte.com",
