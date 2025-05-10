155a70bd417b6317dbc16b7322b3c4406e5179fd
botprzemek <info@botprzemek.pl>
2025-05-10 15:25:53 +0200

    style: fixed public directory

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,5 +2,5 @@ import withNuxt from "./.nuxt/eslint.config.mjs";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default withNuxt(eslintConfigPrettier, {
-    ignores: ["node_modules", "dist", "public", ".nuxt"]
+    ignores: ["node_modules", "dist", "src/public", ".nuxt"]
 });
